73ed550d4f274dbf339a764427ded24484429dc7
Will Sheppard <nobody@example.com>
2019-03-08 14:50:37 +0000

    Read heading from file, read key and email from command line

--- send-me-motd.rb
+++ send-me-motd.rb
@@ -1,24 +1,33 @@
 require 'sendgrid-ruby'
 include SendGrid
 
+require 'date'
+
 # TODOs
 # TODO: No repeats. Keep track of which lines have been sent. Loop at end.
 # TODO: Go through file sequentially, sending one line every time it's run. 
-# TODO: Support custom subject lines.
+# TODO: Support multiple recipients, comma separated
+
+# DONE: Support custom subject lines.
 
 file = ARGV[0]
-abort "usage: #{$0} [filename]" unless file
+sendgrid_key = ARGV[1]
+to_address = ARGV[2]
+abort "usage: #{$0} [filename] [key] [email]" unless file && sendgrid_key
+
+ENV["SENDGRID_API_KEY"] = sendgrid_key
+to_address ||= 'wahahovet@net-list.com'; # default to a test address
 
 # Select a random message
-chosen_line = nil
-File.foreach(file).each_with_index do |line, number|
-    chosen_line = line if rand < 1.0/(number+1)
-end
+lines = File.readlines(file)
+heading = lines.shift
+chosen_line = lines.sample
+chosen_line.chomp!
 
 # Email the message
-from = Email.new(email: 'wahahovet@net-list.com')
-to = Email.new(email: 'wahahovet@net-list.com')
-subject = 'MOTD from '+file;
+from = Email.new(email: to_address)
+to = Email.new(email: to_address)
+subject = heading;
 content = Content.new(type: 'text/plain', value: chosen_line)
 mail = Mail.new(from, subject, to, content)
 
@@ -29,3 +38,6 @@ response = sg.client.mail._('send').post(request_body: mail.to_json)
 #puts response.status_code
 #puts response.body
 #puts response.headers
+
+timestamp = Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')
+puts "#{timestamp}: sent '#{chosen_line}' to #{to_address}"
